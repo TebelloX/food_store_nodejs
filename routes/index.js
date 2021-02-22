7c38b89da01196f9d114ea618b4fe919e0663dca
TebelloX <tebellonamo@gmail.com>
2021-02-22 18:26:33 +0200

    renamed variables.env file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const wes = { name: "Wes", age: 100, cool: true };
+  // res.send("Hey! It works!");
+  // res.json(wes);
+  res.send;
 });
 
 module.exports = router;
